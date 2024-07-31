ba732100696498c19e47e0e76cbe63c9a4af3bf1
dogusyuksel <dogusyuk@gmail.com>
2024-07-31 01:24:32 +0100

    unittest added

---
+++ test/test_25LC512.c
@@ -0,0 +1,22 @@
+#ifdef TEST
+
+#include "unity.h"
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+
+void setUp(void)
+{
+}
+
+void tearDown(void)
+{
+}
+
+void test_MC25LC512_Initilize(void)
+{
+    
+}
+
+#endif // TEST
